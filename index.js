5bc31a11f6086ef8185e28b6e90b24fdce2d0130
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-23 11:21:27 -0800

    commit tree aed9b61beb362e3c057bb34665861a7da08ca5d1

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+    let firstTwo = [];
+    for(let i = 0; i < 2; i++) {
+        firstTwo.push(array[i]);
+    }
+    return firstTwo;
+}
+
+const returnLastTwoDrivers = (array) => {
+    let lastTwo = [];
+    for (let i = array.length-1; i > array.length-3; i--) {
+        lastTwo.unshift(array[i]);
+    }
+    return lastTwo;
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (fare) => {
+    const fareMultiplier = (multiplier) => (fare * multiplier);
+    return fareMultiplier;
+}
+
+const fareDoubler = (fare) => {
+    return fare * 2;
+}
+
+const fareTripler = (fare) => {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(array, fn){
+    return fn(array);
+}
